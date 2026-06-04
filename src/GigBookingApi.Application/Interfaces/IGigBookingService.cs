731dafa89019527b380d3f8a0dd35d2c5b18bf74
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-06-04 20:58:19 +0200

    feat: add new result class in Task generic return type

--- src/GigBookingApi.Application/Interfaces/IGigBookingService.cs
+++ src/GigBookingApi.Application/Interfaces/IGigBookingService.cs
@@ -1,8 +1,9 @@
 ﻿using GigBookingApi.Application.Dtos;
+using GigBookingApi.Application.Results;
 
 namespace GigBookingApi.Application.Interfaces;
 public interface IGigBookingService
 {
-    public Task<IEnumerable<GigBookingReponseModel>> GetAllGigBookings();
-    public Task<GigBookingReponseModel> CreateGigBooking(DateTimeOffset startDate, DateTimeOffset endDate, string street, string streetNumber, string zipCode, string city, string clientName, string clientEmail, string clientPhone);
+    public Task<Result<IEnumerable<GigBookingReponseModel>>> GetAllGigBookings();
+    public Task<Result<GigBookingReponseModel>> CreateGigBooking(DateTimeOffset startDate, DateTimeOffset endDate, string street, string streetNumber, string zipCode, string city, string clientName, string clientEmail, string clientPhone);
 }
