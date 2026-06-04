677edb99ef803a604937583142f6a8e693adf31d
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-06-04 14:23:57 +0200

    feat(get all): Starting my enpoint to get all bookings. Pretty much nothing implemented. Continuing in another branch

--- src/GigBookingApi.Api/Endpoints/GigBookingEnpoints.cs
+++ src/GigBookingApi.Api/Endpoints/GigBookingEnpoints.cs
@@ -7,5 +7,12 @@ public static class GigBookingEnpoints
         var group = app.MapGroup("/api/gigbooking")
             .WithTags("Git Booking Enpoints")
             .WithDescription("Demo on how to use Gig Booking API");
+
+        group.MapGet("/all-bookings", GetAllBookings);
+    }
+
+    private static async Task<IResult> GetAllBookings()
+    {
+        return Results.Ok();
     }
 }
