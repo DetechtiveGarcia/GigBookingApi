9ca68d23e98187808878a5302c7cff97c633fa69
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-06-04 15:32:01 +0200

    refactor(add properties):
    - Add new client properties to GigBooking-entity

--- src/GigBookingApi.Infrastructure/Entities/GigBooking.cs
+++ src/GigBookingApi.Infrastructure/Entities/GigBooking.cs
@@ -1,6 +1,6 @@
 ﻿namespace GigBookingApi.Infrastructure.Entities;
 
-public sealed class GigBooking(DateTime startDate, DateTime endDate, string street, string streetNumber, string zipCode, string city)
+public sealed class GigBooking(DateTime startDate, DateTime endDate, string street, string streetNumber, string zipCode, string city, string clientName, string clientEmail, string clientPhone)
 {
     public string Id { get; init; } = Guid.NewGuid().ToString();
     public DateTime StartDate { get; private set; } = startDate;
@@ -8,9 +8,12 @@ public sealed class GigBooking(DateTime startDate, DateTime endDate, string stre
     public string Street { get; private set; } = street;
     public string StreetNumber { get; private set; } = streetNumber;
     public string ZipCode { get; private set; } = zipCode;
-    public string City { get; private set; } = city; 
+    public string City { get; private set; } = city;
+    public string ClientName { get; private set; } = clientName;
+    public string ClientEmail { get; private set; } = clientEmail;
+    public string ClientPhone { get; private set; } = clientPhone;
 
-    public void UpdateGigBooking(DateTime? startDate, DateTime? endDate, string? street, string? streetNumber, string? zipCode, string? city)
+    public void UpdateGigBooking(DateTime? startDate, DateTime? endDate, string? street, string? streetNumber, string? zipCode, string? city, string? clientName, string? clientEmail, string? clientPhone)
     {
         StartDate = startDate ?? StartDate;
         EndDate = endDate ?? EndDate;
@@ -18,5 +21,8 @@ public sealed class GigBooking(DateTime startDate, DateTime endDate, string stre
         StreetNumber = streetNumber ?? StreetNumber;
         ZipCode = zipCode ?? ZipCode;
         City = city ?? City;
+        ClientName = clientName ?? ClientName;
+        ClientEmail = clientName ?? ClientEmail;
+        ClientPhone = clientPhone ?? ClientPhone;
     }
 }
