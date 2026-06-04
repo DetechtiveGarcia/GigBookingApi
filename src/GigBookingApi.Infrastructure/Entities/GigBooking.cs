bac1bb6d256cd7c75a8be07f0a7851c8b3723bf2
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-06-04 15:24:41 +0200

    feat(update method): update method done

--- src/GigBookingApi.Infrastructure/Entities/GigBooking.cs
+++ src/GigBookingApi.Infrastructure/Entities/GigBooking.cs
@@ -2,11 +2,21 @@
 
 public sealed class GigBooking(DateTime startDate, DateTime endDate, string street, string streetNumber, string zipCode, string city)
 {
-    public string Id { get; private set; } = Guid.NewGuid().ToString();
+    public string Id { get; init; } = Guid.NewGuid().ToString();
     public DateTime StartDate { get; private set; } = startDate;
     public DateTime EndDate { get; private set; } = endDate;
     public string Street { get; private set; } = street;
     public string StreetNumber { get; private set; } = streetNumber;
     public string ZipCode { get; private set; } = zipCode;
-    public string City { get; private set; } = city;
+    public string City { get; private set; } = city; 
+
+    public void UpdateGigBooking(DateTime? startDate, DateTime? endDate, string? street, string? streetNumber, string? zipCode, string? city)
+    {
+        StartDate = startDate ?? StartDate;
+        EndDate = endDate ?? EndDate;
+        Street = street ?? Street;
+        StreetNumber = streetNumber ?? StreetNumber;
+        ZipCode = zipCode ?? ZipCode;
+        City = city ?? City;
+    }
 }
