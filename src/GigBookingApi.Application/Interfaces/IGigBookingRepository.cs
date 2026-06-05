b8267ef10df15c274da4583c7164f00af0028a38
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-06-05 14:11:02 +0200

    chore: added semicolon är det är of Update method

--- src/GigBookingApi.Application/Interfaces/IGigBookingRepository.cs
+++ src/GigBookingApi.Application/Interfaces/IGigBookingRepository.cs
@@ -5,6 +5,6 @@ public interface IGigBookingRepository
 {
     public Task<IEnumerable<GigBookingReponseModel>> GetAllAsync();
     public Task<GigBookingReponseModel> CreateAsync(DateTimeOffset startDate, DateTimeOffset endDate, string street, string streetNumber, string zipCode, string city, string clientName, string clientEmail, string clientPhone);
-    public Task<GigBookingReponseModel> UpdateAsync(string Id, DateTimeOffset startDate, DateTimeOffset endDate, string street, string streetNumber, string zipCode, string city, string clientName, string clientEmail, string clientPhone)
+    public Task<GigBookingReponseModel> UpdateAsync(string Id, DateTimeOffset startDate, DateTimeOffset endDate, string street, string streetNumber, string zipCode, string city, string clientName, string clientEmail, string clientPhone);
     public Task<bool> DeleteAsync(string id);
 }
