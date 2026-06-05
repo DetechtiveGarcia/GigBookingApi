961f9880ad89767d90bdb2c24263b6a06ce848b1
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-06-05 09:49:02 +0200

    feat: same thing for the service as for the repository commit message

--- src/GigBookingApi.Application/Interfaces/IGigBookingService.cs
+++ src/GigBookingApi.Application/Interfaces/IGigBookingService.cs
@@ -6,4 +6,6 @@ public interface IGigBookingService
 {
     public Task<Result<IEnumerable<GigBookingReponseModel>>> GetAllGigBookings();
     public Task<Result<GigBookingReponseModel>> CreateGigBooking(DateTimeOffset startDate, DateTimeOffset endDate, string street, string streetNumber, string zipCode, string city, string clientName, string clientEmail, string clientPhone);
+    public Task<Result<GigBookingReponseModel>> UpdateGigBooking(string id, DateTimeOffset startDate, DateTimeOffset endDate, string street, string streetNumber, string zipCode, string city, string clientName, string clientEmail, string clientPhone);
+    public Task<Result<GigBookingReponseModel>> FindGigBookingById(string id);
 }
