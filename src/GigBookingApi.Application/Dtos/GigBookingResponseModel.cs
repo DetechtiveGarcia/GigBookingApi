0f1824e973557059b8e2084cec9a60738d4c1a1f
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-06-05 18:01:15 +0200

    refactor: changde name of the response dto of a gig booking

--- src/GigBookingApi.Application/Dtos/GigBookingResponseModel.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace GigBookingApi.Application.Dtos;
-
-public sealed record GigBookingReponseModel(string Id, DateTimeOffset StartDate, DateTimeOffset EndDate, string Street, string StreetNumber, string ZipCode, string City, string ClientName, string ClientEmail, string ClientPhone);
