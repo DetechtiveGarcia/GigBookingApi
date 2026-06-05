3b3ebcee910cb91ff37c1b175cc09975ac448fcf
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-06-05 18:02:50 +0200

    refactor: changde name of the gig booking dto return class. Add cancellation token

--- src/GigBookingApi.Application/Interfaces/IGigBookingService.cs
+++ src/GigBookingApi.Application/Interfaces/IGigBookingService.cs
@@ -4,8 +4,8 @@ using GigBookingApi.Application.Results;
 namespace GigBookingApi.Application.Interfaces;
 public interface IGigBookingService
 {
-    public Task<Result<IEnumerable<GigBookingReponseModel>>> GetAllGigBookings();
-    public Task<Result<GigBookingReponseModel>> CreateGigBooking(DateTimeOffset startDate, DateTimeOffset endDate, string street, string streetNumber, string zipCode, string city, string clientName, string clientEmail, string clientPhone);
-    public Task<Result> UpdateGigBooking(string id, DateTimeOffset startDate, DateTimeOffset endDate, string street, string streetNumber, string zipCode, string city, string clientName, string clientEmail, string clientPhone);
-    public Task<Result> DeleteGigBooking(string id);
+    public Task<Result<IEnumerable<GigBookingDto>>> GetAllGigBookings(CancellationToken ct = default);
+    public Task<Result<GigBookingDto>> CreateGigBooking(DateTimeOffset startDate, DateTimeOffset endDate, string street, string streetNumber, string zipCode, string city, string clientName, string clientEmail, string clientPhone, CancellationToken ct = default);
+    public Task<Result> UpdateGigBooking(string id, DateTimeOffset startDate, DateTimeOffset endDate, string street, string streetNumber, string zipCode, string city, string clientName, string clientEmail, string clientPhone, CancellationToken ct = default);
+    public Task<Result> DeleteGigBooking(string id, CancellationToken ct = default);
 }
