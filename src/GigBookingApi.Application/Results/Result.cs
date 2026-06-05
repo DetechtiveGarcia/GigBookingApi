34b50ac836493e9d596c4510c7660baa23e66218
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-06-05 14:11:48 +0200

    feat: add new result non-generic

--- src/GigBookingApi.Application/Results/Result.cs
+++ src/GigBookingApi.Application/Results/Result.cs
@@ -4,3 +4,9 @@ public sealed record Result<T>(bool Succeeded, T? Value = default, string? Error
     public static Result<T> Success(T value) => new(true, Value: value);
     public static Result<T> Fail(string errorMessage) => new(false, ErrorMessage: errorMessage);
 }
+
+public sealed record Result(bool Succeeded, string? ErrorMessage = null)
+{
+    public static Result Success() => new(true);
+    public static Result Fail(string errorMessage) => new(false, ErrorMessage: errorMessage);
+}
