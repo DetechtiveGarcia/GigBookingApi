2cae5809355afb174e14aa203b533e308ae3f4e0
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-06-05 14:04:01 +0200

    feat (id removed): remove id from the record due to it coming from the url.

--- src/GigBookingApi.Api/Dtos/Requests/UpdateGigBookingRequest.cs
+++ src/GigBookingApi.Api/Dtos/Requests/UpdateGigBookingRequest.cs
@@ -1,3 +1,3 @@
 ﻿namespace GigBookingApi.Api.Dtos.Requests;
 
-public sealed record UpdateGigBookingRequest(string Id, DateTimeOffset StartDate, DateTimeOffset EndDate, string Street, string StreetNumber, string ZipCode, string City, string ClientName, string ClientEmail, string ClientPhone);
+public sealed record UpdateGigBookingRequest(DateTimeOffset StartDate, DateTimeOffset EndDate, string Street, string StreetNumber, string ZipCode, string City, string ClientName, string ClientEmail, string ClientPhone);
