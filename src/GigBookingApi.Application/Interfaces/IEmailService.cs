4fd422369a3b5a89fd8622350bd9fad1e39c3926
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-07-25 18:37:21 +0200

    feat:
    - add EmailService signature in Application layer

---
+++ src/GigBookingApi.Application/Interfaces/IEmailService.cs
@@ -0,0 +1,6 @@
+﻿namespace GigBookingApi.Application.Interfaces;
+
+public interface IEmailService
+{
+    Task SendBookingConfirmationAsync(string to, string subject, string htmlBody, CancellationToken ct);
+}
